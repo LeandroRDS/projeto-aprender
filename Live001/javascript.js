f442b1765d2970f5a9e3aa7e2dfc358a836740b9
leandrords <leandrords23012002@gmail.com>
2024-02-18 02:34:23 -0300

    vapuuu

--- Live001/javascript.js
+++ Live001/javascript.js
@@ -1,28 +1,50 @@
+document.getElementById ('id-img-olho').addEventListener('click', clique)
 
+function clique() {
+    const olho = document.getElementById('id-img-olho')
+    const iris = document.getElementById('id-img-iris')
+    const input = document.getElementById('id-input-senha')
 
-  document.getElementById ('id-img-olho').addEventListener('click', clique)
-  function clique(){
-    if( document.getElementById('id-input-senha').type === 'password'){
-        document.getElementById('id-input-senha').type = 'text'
-        document.getElementById('id-img-olho').src = 'imagens/olho_aberto.png'
-        document.getElementById('id-img-iris').src = 'imagens/iris.png'
+    if (input.type === 'password') {
+        input.type = 'text'
+        olho.src = 'imagens/olho_aberto.png'
+        iris.src = 'imagens/iris.png'
+        iris.style.display = 'block'
 
-        const iris = document.getElementById('id-img-iris');
+        const { width, height } = olho.getBoundingClientRect()
+        const centerX = width / 2
+        const centerY = height / 2
 
         document.addEventListener("mousemove", (e) => {
-          const cursorX = e.clientX;
-          const cursorY = e.clientY;
-    
-          // Posicionar a bolinha na posição do cursor
-          iris.style.left = `${cursorX}px`;
-          iris.style.top = `${cursorY}px`;
-});
+            const cursorX = e.clientX -196 
+            const cursorY = e.clientY - 53
 
-    }else{
-        document.getElementById('id-input-senha').type = 'password'
-        document.getElementById('id-img-olho').src = 'imagens/olho_fechado.png'
-        document.getElementById('id-img-iris').src = ''
+            // Calculate the distance between the cursor and the center of the eye
+            const distance = Math.sqrt(Math.pow(cursorX - centerX, 2) + Math.pow(cursorY - centerY, 2))
+
+            // If the cursor is within a radius of 100px around the center of the eye
+            if (distance <= 1000) {
+                // Calculate the angle between the center of the eye and the cursor position
+                const angle = Math.atan2(cursorY - centerY, cursorX - centerX)
+
+                // Calculate the new position of the iris
+                const newX = centerX + Math.cos(angle) * (width / 20)
+                const newY = centerY + Math.sin(angle) * (height / 10)
+
+                // Position the iris
+                iris.style.left = `${newX}px`
+                iris.style.top = `${newY}px`
+            }
+        })
+
+    } else {
+        input.type = 'password'
+        olho.src = 'imagens/olho_fechado.png'
+        iris.src = ''
+        iris.style.display = 'none'
+
+        document.removeEventListener("mousemove", (e) => {
+            // code here
+        })
     }
-        
-  };
-
+}

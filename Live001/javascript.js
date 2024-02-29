46ff48f815ef586c70dd3210eca367cf65be2cdf
leandrords <leandrords23012002@gmail.com>
2024-02-29 01:59:20 -0300

    verção final

--- Live001/javascript.js
+++ Live001/javascript.js
@@ -12,24 +12,24 @@ function clique() {
         iris.style.display = 'block'
 
         const { width, height } = olho.getBoundingClientRect()
-        const centerX = width / 2
-        const centerY = height / 2
+        const centerX = width / 2 
+        const centerY = height / 2 -5
 
         document.addEventListener("mousemove", (e) => {
-            const cursorX = e.clientX -196 
-            const cursorY = e.clientY - 53
+            const cursorX = e.clientX 
+            const cursorY = e.clientY 
 
             // Calculate the distance between the cursor and the center of the eye
             const distance = Math.sqrt(Math.pow(cursorX - centerX, 2) + Math.pow(cursorY - centerY, 2))
 
             // If the cursor is within a radius of 100px around the center of the eye
-            if (distance <= 1000) {
+            if (distance === distance) {
                 // Calculate the angle between the center of the eye and the cursor position
-                const angle = Math.atan2(cursorY - centerY, cursorX - centerX)
+                const angle = Math.atan2(cursorY - centerY -280, cursorX - centerX - 940)
 
                 // Calculate the new position of the iris
-                const newX = centerX + Math.cos(angle) * (width / 20)
-                const newY = centerY + Math.sin(angle) * (height / 10)
+                const newX = centerX + Math.cos(angle) * (width / 14)
+                const newY = centerY + Math.sin(angle) * (height / 9)
 
                 // Position the iris
                 iris.style.left = `${newX}px`
